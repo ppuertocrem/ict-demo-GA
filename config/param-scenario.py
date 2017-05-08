a832440f32268f79ffd6193c7557d91d8e3c5e8b
Puerto Pablo <pablo.puerto@crem.ch>
2017-05-08 18:26:10 +0200

    Create param-scenario.py

---
+++ config/param-scenario.py
@@ -0,0 +1,14 @@
+# Central heat-pump and thermal network
+t_set_network = 20  # [deg.C]
+t_lake = 10  # [deg.C]
+flow_network = 0.5  # [m3/s]
+
+# Heating heat-pump and heating needs
+t_heating = 70  # [deg.C]
+flow_heating = 1.0  # [m3/s]
+flow_heating_sec = 1.0  # [m3/s]
+
+# Cooling heat-pump and cooling needs
+t_cooling = 2  # [deg.C]
+flow_cooling = 0.5  # [m3/s]
+flow_cooling_sec = 0.5  # [m3/s]
